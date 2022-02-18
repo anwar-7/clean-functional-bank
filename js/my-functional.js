086d2ab5b83606999cf0630103fc1278801cd802
anwarul1 <anwarjewel007@gmail.com>
2022-02-18 21:32:53 +0600

    update code

--- js/my-functional.js
+++ js/my-functional.js
@@ -5,6 +5,12 @@ function gatInputValue(fieldId) {
   return value;
 }
 
+function getInnerTextValue(fieldId) {
+  const inputInnerTextValue = document.getElementById(fieldId);
+  const value = parseFloat(inputInnerTextValue.innerText);
+  return value;
+}
+
 function updateTotal(fieldId, amount) {
   const totalTag = document.getElementById(fieldId);
   const previousTotal = parseFloat(totalTag.innerText);
@@ -12,13 +18,37 @@ function updateTotal(fieldId, amount) {
   totalTag.innerText = currentTotal;
 }
 
+function updateBalance(amount, isAdding) {
+  const balanceTag = document.getElementById('balance-total');
+  const previousBalance = parseFloat(balanceTag.innerText);
+  let currentBalance;
+  if (isAdding == true) {
+    currentBalance = previousBalance + amount;
+  } else if (isAdding == false) {
+    currentBalance = previousBalance - amount;
+  }
+  balanceTag.innerText = currentBalance;
+}
+
 // handle deposit button
 document
   .getElementById('deposit-button')
   .addEventListener('click', function () {
     const amount = gatInputValue('deposit-input');
-    updateTotal('deposit-total', amount);
+    if (amount > 0) {
+      updateTotal('deposit-total', amount);
+      updateBalance(amount, true);
+    }
   });
 
 // handle withdraw button
-document.getElementById('');
+document
+  .getElementById('withdraw-button')
+  .addEventListener('click', function () {
+    const amount = gatInputValue('withdraw-input');
+    const balance = getInnerTextValue('balance-total');
+    if (amount > 0 && balance > amount) {
+      updateTotal('withdraw-total', amount);
+      updateBalance(amount, false);
+    }
+  });
